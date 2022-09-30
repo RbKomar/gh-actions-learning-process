f4733ea365cc4fce1865cf300f36f61bb3d2468f
Robert <RobKomar98@gmail.com>
2022-09-30 15:29:29 +0200

    fix: lack of import os

--- sample.py
+++ sample.py
@@ -1,6 +1,7 @@
 from datetime import datetime
+import os
 
 now = datetime.now()
 with open("README.md", 'w+') as f:
     f.write(now.strftime("%Y-%m-%d %H:%M:%S"))
-    f.write(os.environ["README.md"])
+    #f.write(os.environ["README.md"])
