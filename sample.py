29d608805b273670bb3c6876ee19c73a130764fa
Robert <RobKomar98@gmail.com>
2022-09-30 16:26:44 +0200

    testing venv in python

--- sample.py
+++ sample.py
@@ -4,4 +4,4 @@ import os
 now = datetime.now()
 with open("README.md", 'w+') as f:
     f.write(now.strftime("%Y-%m-%d %H:%M:%S"))
-    #f.write(os.environ["README.md"])
+    f.write(os.environ["TEST_ENV"])
